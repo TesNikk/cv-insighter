acaafeae70ea5944378659d38642ef8b68e5b92c
Nahid Niyaz Shovon <nahidniyaz185@gmail.com>
2025-03-14 00:17:29 +0600

    fix: Slight css changes

--- src/app/components/CVForm.tsx
+++ src/app/components/CVForm.tsx
@@ -24,7 +24,7 @@ const CVForm = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto bg-gray-200 p-6 rounded-lg shadow-lg m-4">
+    <div className="max-w-2xl mx-auto bg-gray-200 p-6 rounded-lg shadow-lg my-4">
       <h2 className="text-2xl font-bold mb-4 text-center">Create Your CV</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
         
